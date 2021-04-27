3a7866ff1fa1226650c59db4d98373b101725bdf
myeongju_ji <myeongju_ji@tmax.co.kr>
2021-04-27 21:36:50 +0900

    change java 14 to 8

--- src/test/java/com/kakaopay/investment/user/UserTests.java
+++ src/test/java/com/kakaopay/investment/user/UserTests.java
@@ -1,7 +1,8 @@
 package com.kakaopay.investment.user;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
@@ -22,13 +23,13 @@ public class UserTests extends InvestmentApplicationTests {
         EUser origin = EUser.builder()
                 .build();
         EUser saved = rUser.save(origin);
-
+        assertNotNull(saved);
 
         // when
         Optional<EUser> found= rUser.findById(origin.getUserId());
 
         // then
-        assertFalse(found.isEmpty());
+        assertTrue(found.isPresent());
         EUser user = found.get();
         assertEquals(origin.getUserId(), user.getUserId());
 
