43f52fbfb1d2c2e5f34a997ce07fc2d1481d6479
DakOoon <wlaudwn007@gmail.com>
2021-04-30 00:40:55 +0900

    change DateTimeFormat

--- src/main/java/com/kakaopay/investment/util/DateTimeUtils.java
+++ src/main/java/com/kakaopay/investment/util/DateTimeUtils.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtils {
-    public static final DateTimeFormatter PATTERN = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+    public static final DateTimeFormatter PATTERN = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static String format(LocalDateTime target) {
         if(target == null) {
