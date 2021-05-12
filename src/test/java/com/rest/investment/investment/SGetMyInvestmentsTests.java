4ce08c4d9584c78a62ab0ca3ef626164d8180637
myeongju_ji <myeongju_ji@tmax.co.kr>
2021-05-12 19:31:49 +0900

    dev SGetMyInvestment

--- src/test/java/com/rest/investment/investment/SGetMyInvestmentsTests.java
+++ src/test/java/com/rest/investment/investment/SGetMyInvestmentsTests.java
@@ -1,5 +1,32 @@
 package com.rest.investment.investment;
 
-public class SGetMyInvestmentsTests {
+import java.util.concurrent.TimeUnit;
 
+import com.rest.investment.InvestmentApplicationTests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+public class SGetMyInvestmentsTests extends InvestmentApplicationTests {
+
+    @MockBean
+    private RInvestment rInvestment;
+
+    @Autowired
+    private SGetMyInvestments sGetMyInvestments;
+
+    @Test
+    @Timeout(value = 1000L, unit = TimeUnit.MILLISECONDS)
+    @DisplayName("SGetMyInvestmentsTests: SGetMyInvestments")
+    public void SGetMyInvestments() {
+        /* given */
+        
+        /* when */
+
+        /* then */
+
+    }
 }
